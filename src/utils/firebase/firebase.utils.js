866a2676de281c002912db1dfcf13bd0989322f1
thanthanhduy <thanthanhduy.work@gmail.com>
2022-06-01 13:50:38 +0700

    [DuyTT] add domain

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,16 +1,19 @@
-import firebase from 'firebase/compat/app' // version 9
-import 'firebase/compat/auth'
+import firebase from "firebase/compat/app"; // version 9
+import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBds-Fo2CH6nhfCJHHXFrJD7-AEuiczSN4',
-  authDomain: 'se-capstone-project-management.firebaseapp.com',
-  projectId: 'se-capstone-project-management',
-  storageBucket: 'se-capstone-project-management.appspot.com',
-  messagingSenderId: '336488913408',
-  appId: '1:336488913408:web:eaf4fa9ba441c2ea81d6e5'
-}
+  apiKey: "AIzaSyBds-Fo2CH6nhfCJHHXFrJD7-AEuiczSN4",
+  authDomain: [
+    "se-capstone-project-management.firebaseapp.com",
+    "se-capstone-project-management.vercel.app"
+  ],
+  projectId: "se-capstone-project-management",
+  storageBucket: "se-capstone-project-management.appspot.com",
+  messagingSenderId: "336488913408",
+  appId: "1:336488913408:web:eaf4fa9ba441c2ea81d6e5"
+};
 
-firebase.initializeApp(firebaseConfig)
-var auth = firebase.auth()
-var provider = new firebase.auth.GoogleAuthProvider()
-export { auth, provider }
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
+var provider = new firebase.auth.GoogleAuthProvider();
+export { auth, provider };
